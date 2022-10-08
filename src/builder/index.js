ec3c7a274ef92d721767e58ad93f803a5bc81627
Egor Kolesnikov <freethinkel@gmail.com>
2022-10-08 20:25:45 +0600

    fix build & update api

--- src/builder/index.js
+++ src/builder/index.js
@@ -1,34 +1,34 @@
-const vite = require('vite');
-const process = require('process');
-const fs = require('fs');
-const path = require('path');
+const vite = require("vite");
+const process = require("process");
+const fs = require("fs");
+const path = require("path");
 
 const build = ({ watch = false }) => {
-	const projectDir = process.cwd();
-	const projectPackegeJson = JSON.parse(
-		fs.readFileSync(path.join(projectDir, 'package.json'))
-	);
-	const entryFile = path.join(projectDir, projectPackegeJson.main);
+  const projectDir = process.cwd();
+  const projectPackegeJson = JSON.parse(
+    fs.readFileSync(path.join(projectDir, "package.json"))
+  );
+  const entryFile = path.join(projectDir, projectPackegeJson.main);
 
-	vite.build({
-		build: {
-			lib: {
-				entry: entryFile,
-				name: 'plugin',
-				fileName: projectPackegeJson.name,
-			},
-			watch: true,
-			cssCodeSplit: false,
-			assetsDir: '',
-			outDir: 'build',
-			rollupOptions: {},
-		},
-	});
+  vite.build({
+    build: {
+      lib: {
+        entry: entryFile,
+        name: "plugin",
+        fileName: projectPackegeJson.name,
+      },
+      watch,
+      cssCodeSplit: false,
+      assetsDir: "",
+      outDir: "build",
+      rollupOptions: {},
+    },
+  });
 };
 
 const dev = () => build({ watch: true });
 
 module.exports = {
-	dev,
-	build,
+  dev,
+  build,
 };
--- src/index.d.ts
+++ src/index.d.ts
@@ -1,50 +1,50 @@
-import * as tauri from '@tauri-apps/api';
-import { Axios } from 'axios';
+import * as tauri from "@tauri-apps/api";
+import { Axios } from "axios";
 
 interface Documents {
-	subscribe(cb: (documents: Array<Document>) => void): () => void;
-	add(document: Document): void;
-	addAll(documents: Array<Document>): void;
-	remove(document: Document): void;
-	removeAll(): void;
-	get(): Array<Document>;
-	update(callback: (state: Array<Document>) => Array<Document>): void;
+  subscribe(cb: (documents: Array<Document>) => void): () => void;
+  add(document: Document): void;
+  addAll(documents: Array<Document>): void;
+  remove(document: Document): void;
+  removeAll(): void;
+  get(): Array<Document>;
+  update(callback: (state: Array<Document>) => Array<Document>): void;
 }
 
 interface Sidebar {
-	label: string | Record<string, string>;
+  label: string | Record<string, string>;
 }
 
 export class PluginProvider {
-	documents: Documents;
-	sidebar?: Sidebar;
-	onInit?(): void;
-	onUpdateDocument?(document: Document): void;
-	onAddNewDocument?(document?: Document): void;
-	changeDocumentPosition?(document: Document): void;
-	removeDocument?(document: Document): void;
-	onAddGroup?(): void;
-	onRenameDocument?(document: Document): void;
-	onRenameFolder?(document: Document): void;
+  documents: Documents;
+  sidebar?: Sidebar;
+  onInit?(): void;
+  onUpdateDocument?(document: Document): void;
+  onAddNewDocument?(document?: Document): void;
+  changeDocumentPosition?(document: Document): void;
+  removeDocument?(document: Document): void;
+  onAddGroup?(): void;
+  onRenameDocument?(document: Document): void;
+  onRenameFolder?(document: Document): void;
 }
 
 export interface Document {
-	id: string;
-	name: string;
-	content?: string;
-	position?: number;
-	type?: 'document' | 'folder';
-	parentId?: string;
-	children?: Document[];
+  id: string;
+  name: string;
+  position?: number;
+  type?: "document" | "folder";
+  parentId?: string;
+  children?: Document[];
+  getContent(): string | Promise<string>;
 }
 
 export interface PluginApi {
-	fetch: Axios;
-	platform: typeof tauri.os.platform;
-	shell: typeof tauri.shell;
-	notification: {
-		show: typeof tauri.notification.sendNotification;
-	};
-	clipboard: typeof tauri.clipboard;
-	dialog: typeof tauri.dialog;
+  fetch: Axios;
+  platform: typeof tauri.os.platform;
+  shell: typeof tauri.shell;
+  notification: {
+    show: typeof tauri.notification.sendNotification;
+  };
+  clipboard: typeof tauri.clipboard;
+  dialog: typeof tauri.dialog;
 }
